889ccff5d12440a8f3ad78e3b5c8f7ce3ac7e2fa
Martin Shaw <martin.shaw@meanbee.com>
2012-01-23 14:13:14 +0000

    Added some functionality to show the form if an address has already been inputted

--- code/Block/Customer/Address/Edit.php
+++ code/Block/Customer/Address/Edit.php
@@ -13,4 +13,24 @@ class Meanbee_Postcode_Block_Customer_Address_Edit extends Mage_Customer_Block_A
             return parent::getTemplate();
         }
     }
+
+    public function hasAddress() {
+        $hasAddress = true;
+
+        $this->hasAddressField($this->getAddress()->getPostcode(), $hasAddress);
+        $this->hasAddressField($this->getAddress()->getStreet(), $hasAddress);
+        $this->hasAddressField($this->getAddress()->getCity(), $hasAddress);
+
+        return $hasAddress;
+    }
+
+    protected function hasAddressField($field, &$hasAddressFlag) {
+        if ($hasAddressFlag == false) {
+            return;
+        }
+
+        if ($field == NULL) {
+            $hasAddressFlag = false;
+        }
+    }
 }
