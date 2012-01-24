704e2ab239bef3061c5c6368f9fea664740a303a
Martin Shaw <martin.shaw@meanbee.com>
2012-01-24 13:10:00 +0000

    Fixed a typo.

--- code/Block/Customer/Address/Edit.php
+++ code/Block/Customer/Address/Edit.php
@@ -4,7 +4,7 @@ class Meanbee_Postcode_Block_Customer_Address_Edit extends Mage_Customer_Block_A
      * Override of the getTemplate method to change it to one based on our magento version
      */
     public function getTemplate() {
-        if (Mage::help('postcode')->isEnabled()) {
+        if (Mage::helper('postcode')->isEnabled()) {
             $version = Mage::helper('postcode')->getVersion();
             if ($version != false) {
                 return 'meanbee/postcode/' . $version . '/customer_address_edit.phtml';
