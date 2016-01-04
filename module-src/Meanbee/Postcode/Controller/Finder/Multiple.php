035d6165a2966d536a99343f829f4a9d5f672f75
Nathan McBride <nathan@brideo.co.uk>
2016-01-04 17:14:59 +0000

    Refactor controllers to be more inline with Magento2
    We shouldn't be setting a json response manually, also the `Finder`
    class should be abstract and have some default functionality.

--- module-src/Meanbee/Postcode/Controller/Finder/Multiple.php
+++ module-src/Meanbee/Postcode/Controller/Finder/Multiple.php
@@ -1,27 +1,25 @@
 <?php
 namespace Meanbee\Postcode\Controller\Finder;
 
-class Multiple extends \Meanbee\Postcode\Controller\Finder
+use Meanbee\Postcode\Controller\Finder;
+
+class Multiple extends Finder
 {
 
+    /**
+     * Set the json response from the Postcode Anywhere
+     * api call response.
+     *
+     * @return $this
+     */
     public function execute()
     {
-        header("Content-type: application/json");
+        $result = $this->resultJsonFactory->create();
 
-        $postcode = strtolower(preg_replace("/[^a-zA-Z0-9]/", "", str_replace(' ', '', $_GET['postcode'])));
-        $area = $_GET['area'];
-
-        if (!empty($postcode)) {
-            /** @var Meanbee_Postcode_Model_Call $call */
-            $call = Mage::getModel('postcode/call');
-            echo $call->findMultipleByPostcode($postcode, $area);
-        } else {
-            echo Zend_Json::encode(array(
-                "error"   => true,
-                "content" => "No postcode provided"
-            ));
-        }
-        exit;
+        return $result->setData($this->call->findMultipleByPostcode(
+            $this->getRequest()->getParam('postcode'),
+            $this->getRequest()->getParam('area')
+        ));
     }
 
 }
